f5bd2aa9dd703b361048c3ee322a390df8779c6d
Matheus Silva <matheus.silva@in8.com.br>
2024-01-27 21:15:08 -0300

    feat: conditional rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-  import type { ShowPropsProps } from "./@types/ShowProps";
-import ShowProps from "./lib/ShowProps.svelte";
+  import Counter from "./lib/Counter.svelte";
+
 
   let src = "./src/assets/svelte.svg";
   let text = "<h2>state</h2>";
-
-  let propsToShow: ShowPropsProps = {
-    number: 10,
-    text: "hii"
-  }
 </script>
 
 <img {src} alt="svelte" />
 <h1>{@html text}</h1>
-<ShowProps props={propsToShow} />
+
+<Counter />
 
 <style>
   h1 {
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -2,9 +2,6 @@
   let count: number = 0;
   $: doubleCount = count * 2
   $: {
-    if(count > 20) {
-      console.log("passou de 20 kkkkkkkkkkk ce ta doooido")
-    }
     console.log(count)
   }
 
@@ -13,9 +10,14 @@
   };
 </script>
 
-<h3>count is {count > 0 ? count : "empty!"}</h3>
-<h3>{doubleCount}</h3>
 <button on:click={increment}>increment!</button>
 <button on:click={() => count--}>decrement!</button>
-<br/>
-<br/>
+
+<h3>count is {count > 0 ? count : "empty!"}</h3>
+<h3>doubled: {doubleCount}</h3>
+
+{#if count > 10}
+<div>count passou de 10!!!!!!!! insano demais!</div>
+{:else}
+<div>count nao passou dos 10 ainda kkkkkkkkkkk NEWBAO</div>
+{/if}
